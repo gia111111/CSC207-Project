befeb77bbbafb73c9c68ff0765b1eeb68a452c71
chenvi24 <victoriaj.chen@mail.utoronto.ca>
2024-11-17 14:04:37 -0500

    add profile view

---
+++ lab-5-main/src/main/java/interface_adapter/profile/ProfileController.java
@@ -0,0 +1,15 @@
+package interface_adapter.profile;
+
+import interface_adapter.ViewManagerModel;
+
+public class ProfileController {
+    private final ViewManagerModel viewManagerModel;
+
+    public ProfileController(ViewManagerModel viewManagerModel) {
+        this.viewManagerModel = viewManagerModel;
+    }
+
+//    void switchToDashboardView(){
+//
+//    }
+}
---
+++ lab-5-main/src/main/java/interface_adapter/profile/ProfileViewModel.java
@@ -0,0 +1,34 @@
+package interface_adapter.profile;
+
+import interface_adapter.ViewModel;
+
+public class ProfileViewModel extends ViewModel<ProfileState>{
+    public static final String TITLE_LABEL = "Profile";
+    public static final String SECTION_ONE = "Basic Information";
+    public static final String ONE_QONE = "Question 1 text: ";
+    public static final String ONE_QTWO = "Question 2 text: ";
+    public static final String SECTION_TWO = "Section 2";
+    public static final String TWO_QONE = "Question 1 text: ";
+    public static final String TWO_QTWO = "Question 2 text: ";
+    public static final String SECTION_THREE = "Section 3";
+    public static final String THREE_QONE = "Question 1 text: ";
+    public static final String THREE_QTWO = "Question 2 text: ";
+    public static final String SECTION_FOUR = "Section 4";
+    public static final String FOUR_QONE = "Question 1 text: ";
+    public static final String FOUR_QTWO = "Question 2 text: ";
+    public static final String SECTION_FIVE = "Section 5";
+    public static final String FIVE_QONE = "Question 1 text: ";
+    public static final String FIVE_QTWO = "Question 2 text: ";
+    public static final String SAVE_BUTTON_LABEL = "Save";
+
+    public static final String MC_A = "A";
+    public static final String MC_B = "B";
+    public static final String MC_C = "C";
+    public static final String MC_D = "D";
+    public static final String MC_E = "E";
+
+    public ProfileViewModel() {
+        super("profile");
+        setState(new ProfileState());
+    }
+}
