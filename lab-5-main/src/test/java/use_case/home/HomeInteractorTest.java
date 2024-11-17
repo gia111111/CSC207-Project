a120c0d2f4c2f9812dc974596fa9af87d632d320
abigail <abigail.chen@mail.utoronto.ca>
2024-11-17 13:41:17 -0500

    hometest

---
+++ lab-5-main/src/test/java/use_case/home/HomeInteractorTest.java
@@ -0,0 +1,16 @@
+package use_case.home;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
+import org.junit.jupiter.api.Test;
+import use_case.signup.*;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HomeInteractorTest {
+}
+
