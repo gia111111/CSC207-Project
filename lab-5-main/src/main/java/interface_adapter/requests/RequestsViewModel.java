415887a19ae4fbec169260a665e7de2d9842c397
abigail <abigail.chen@mail.utoronto.ca>
2024-11-17 23:48:17 -0500

    request view and interface adapter

--- lab-5-main/src/main/java/interface_adapter/requests/RequestsViewModel.java
+++ lab-5-main/src/main/java/interface_adapter/requests/RequestsViewModel.java
@@ -6,6 +6,7 @@ import interface_adapter.ViewModel;
 public class RequestsViewModel extends ViewModel <RequestsState> {
     public static final String VIEW_PROFILE_BUTTON_LABEL = "View Profile";
     public static final String BACK_BUTTON_LABEL = "Back";
+//    public static final String USERNAME_LABEL = "user"
 
 
     public RequestsViewModel() {
--- lab-5-main/src/main/java/view/RequestsView.java
+++ lab-5-main/src/main/java/view/RequestsView.java
@@ -1,5 +1,6 @@
 package view;
 
+import entity.Request;
 import interface_adapter.dashboard.DashBoardViewModel;
 import interface_adapter.profile.ProfileController;
 import interface_adapter.profile.ProfileViewModel;
@@ -12,37 +13,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.List;
 
 public class RequestsView extends JPanel implements ActionListener, PropertyChangeListener{
     private final String viewName = "requests";
     private final RequestsViewModel requestsViewModel;
     private final RequestsController requestController;
-
-    private final JButton viewProfile;
+    private final List<Request> requests;
+    private JButton viewProfile;
     private final JButton back;
 
-    public RequestsView(RequestsController requestController, RequestsViewModel requestsViewModel){
+    public RequestsView(RequestsController requestController, RequestsViewModel requestsViewModel, List<Request> requests){
         this.requestsViewModel = requestsViewModel;
         this.requestController = requestController;
+        this.requests =requests;
         this.requestsViewModel.addPropertyChangeListener(this);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         final JLabel title = new JLabel("Requests");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-
-        final JPanel buttons = new JPanel();
-        viewProfile = new JButton(requestsViewModel.VIEW_PROFILE_BUTTON_LABEL);
-        buttons.add(viewProfile);
-
-        back = new JButton(requestsViewModel.BACK_BUTTON_LABEL);
-        buttons.add(back);
-        buttons.setLayout(new BoxLayout(buttons,BoxLayout.X_AXIS));
-
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
         this.add(title);
 
-        this.add(buttons);
+        for(int i = 1; i <= requests.size(); i++) {
+            final JPanel request = new JPanel();
+            Request requesti = requests.get(i);
+            JLabel username = new JLabel(requesti.getName());
+            JLabel score = new JLabel(String.valueOf(requesti.getScores()));
+            JLabel status = new JLabel(String.valueOf(requesti.getStatus()));
+            viewProfile = new JButton(requestsViewModel.VIEW_PROFILE_BUTTON_LABEL);
+            request.add(username);
+            request.add(score);
+            request.add(status);
+            request.add(viewProfile);
+            request.setLayout(new BoxLayout(request,BoxLayout.X_AXIS));
+            this.add(request);
+        }
+
+        back = new JButton(requestsViewModel.BACK_BUTTON_LABEL);
+        this.add(back);
+        // Add action listener for the "View profile" button
+
+
+
+        // Add action listener for the "back" button
+        back.addActionListener(e -> {
+            if (requestController != null) {
+                requestController.switchToDashboard();
+            }
+        });
 
     }
 
