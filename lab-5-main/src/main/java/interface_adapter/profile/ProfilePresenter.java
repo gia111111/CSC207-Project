5435cbb218ab5f4fc1a9081f476c0ab3e5259dcd
Qifan Yang <151076874+gia111111@users.noreply.github.com>
2024-11-19 13:12:45 -0500

    create profile interface adapter

---
+++ lab-5-main/src/main/java/interface_adapter/profile/ProfilePresenter.java
@@ -0,0 +1,45 @@
+package interface_adapter.profile;
+
+import interface_adapter.ViewManagerModel;
+import use_case.createProfile.CreateProfileOutputBoundary;
+import use_case.createProfile.CreateProfileOutputData;
+
+import javax.swing.*;
+
+/**
+ * The Presenter for the Create Profile Use Case.
+ */
+public class ProfilePresenter implements CreateProfileOutputBoundary {
+
+    private final ProfileViewModel profileViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public ProfilePresenter(ProfileViewModel profileViewModel, ViewManagerModel viewManagerModel) {
+        this.profileViewModel = profileViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    /**
+     * If successful, switch to the dashboard view.
+     * @param outputData the output data
+     */
+    @Override
+    public void prepareSuccessView(CreateProfileOutputData outputData) {
+        final ProfileState profileState = profileViewModel.getState();
+        this.profileViewModel.setState(profileState);
+        profileViewModel.firePropertyChanged();
+
+        viewManagerModel.setState("dashboard");
+    }
+
+    public void prepareFailView(String error) {
+        final ProfileState profileState = profileViewModel.getState();
+        profileState.setErrorMessage(error);
+        profileViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void switchToDashBoardView() {
+
+    }
+}
--- lab-5-main/src/main/java/interface_adapter/profile/ProfileState.java
+++ lab-5-main/src/main/java/interface_adapter/profile/ProfileState.java
@@ -1,9 +1,20 @@
 package interface_adapter.profile;
 
+import java.util.List;
+
+/**
+ * The state for the Profile View Model.
+ */
 public class ProfileState {
 
     private String currentView = "";
     private String errorMessage;
+    private String name;
+    private String gender;
+    private String sexualOrientation;
+    private int age;
+    private List<String> questions;
+    private List<String> answers;
 
     public String getCurrentView() {
         return currentView;
@@ -11,6 +22,30 @@ public class ProfileState {
 
     public String getErrorMessage() {
         return errorMessage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getSexualOrientation() {
+        return sexualOrientation;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getQuestions() {
+        return questions;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
     }
 
     public void setCurrentView(String currentView) {
@@ -21,10 +56,41 @@ public class ProfileState {
         this.errorMessage = errorMessage;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSexualOrientation(String sexualOrientation) {
+        this.sexualOrientation = sexualOrientation;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setQuestions(List<String> questions) {
+        this.questions = questions;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+
+    @Override
     public String toString() {
         return "ProfileState{"
                 + "currentView='" + currentView + '\''
                 + ", errorMessage='" + errorMessage + '\''
+                + ", name='" + name + '\''
+                + ", gender=" + gender
+                + ", sexualOrientation='" + sexualOrientation + '\''
+                + ", age=" + age
+                + ", questions=" + questions
+                + ", answers=" + answers
                 + '}';
     }
 }
