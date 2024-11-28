f24cef557236c5a6750fcddb449797633bc31b92
kzyanj <kensleyzhouyanjun023@gmail.com>
2024-11-28 01:30:24 -0500

    Find Interactor

---
+++ lab-5-main/src/main/java/use_case/find/FindProfileInputData.java
@@ -0,0 +1,26 @@
+package use_case.find;
+
+import data_access.RemoteDataAccessObject;
+
+import java.util.List;
+import java.util.Map;
+
+public class FindProfileInputData {
+    private final String name;
+    private final Map<String, Double> scores;
+    private final Map<String, Boolean> matches;
+    private final RemoteDataAccessObject remoteDataAccessObject;
+
+    public FindProfileInputData(Map<String, Double> scores, Map<String, Boolean> matches, RemoteDataAccessObject remoteDataAccessObject) {
+        this.name = remoteDataAccessObject.getCurrentUsername();
+        this.scores = scores;
+        this.matches = matches;
+        this.remoteDataAccessObject = remoteDataAccessObject;
+    }
+
+    public String getName() { return name;}
+
+    public Map<String, Double> getScores() {return scores;}
+
+    public Map<String, Boolean> getMatches() {return matches;}
+}
