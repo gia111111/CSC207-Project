88e0f493c3405927d35a6c05b44b20968e29eb0b
kzyanj <kensleyzhouyanjun023@gmail.com>
2024-12-03 23:04:31 -0500

    Abi's Test

--- Code&Cupid/src/test/java/use_case/requests/RequestsInteractorTest.java
+++ Code&Cupid/src/test/java/use_case/requests/RequestsInteractorTest.java
@@ -8,7 +8,10 @@ import entity.Requests;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
